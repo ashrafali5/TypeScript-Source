1b647fd0819d00235fba1dc5cbe5bc70fbb4ddf5
Ashraf Ali <ashrafmohd83517@gmail.com>
2025-05-31 07:40:44 +0530

    commit

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,5 @@
 "use strict";
-// ---- TypeScript DATA TYPES ----
+// ! ---- TypeScript DATA TYPES ----
 // let a: number; // number will store
 // let b: string; // string will store
 // let c: boolean; // boolean will store
@@ -30,14 +30,95 @@
 // const func: userName = (n, m) => {
 //   return n * m;
 // };
-// ---- TypeScript ARRAY ----
+// ! ---- TypeScript ARRAY ----
 // const arr: number[] = [34, 4, 63, 64]; // Ok
 // const arr1: number[] = [34, "hello", 63]; // Error: number must
 // const arr2: string[] = ["apple", "mango", "banana"]; // Ok
 // const arr3: string[] = ["apple", true]; // Error: string must
 // const arr4: Array<string> = []; // Good Form Of Array
 // const arr5: Array<string | number | boolean> = []; // All String,Number,Boolean
-const arr6 = [44, 66, "mango"];
-console.log(arr6);
-arr6[0] = "Ashraf Ali";
-console.log(arr6);
+// const arr6: Array<number | string> = [44, 66, "mango"];
+// console.log(arr6);
+// arr6[0] = "Ashraf Ali";
+// console.log(arr6);
+// const arr7: Array<number | string> = new Array(5);
+// arr7[0] = 10;
+// arr7[1] = 20;
+// arr7[2] = 30;
+// arr7[3] = 40;
+// arr7[4] = "Hello";
+// console.log(arr7);
+// ! ---- TypeScript Objects ----
+// type Obj = {
+//   height: number;
+//   width: number;
+//   gender?: boolean; // TODO:  ? = optional
+// };
+// const obj: Obj = {
+//   height: 34,
+//   width: 432,
+//   gender: false,
+// };
+// const obj1: Obj = {
+//   height: 43,
+//   width: 4532,
+// };
+// TODO: Interface
+// interface Obj {
+//   height: number;
+//   width: number;
+//   gender?: boolean;
+// }
+// type FuncType = (n: number, m: number) => void;
+// interface newObj extends Obj {
+//   name: string;
+//   age: number;
+//   isMarried?: boolean;
+//   func?: FuncType; // Optional function property
+// }
+// const object: newObj = {
+//   name: "Ashraf Ali",
+//   age: 23,
+//   height: 5.8,
+//   width: 2.5,
+//   isMarried: false,
+//   func: (n, m) => {
+//     console.log(n * m);
+//   },
+// };
+// object.func?.(10, 50); // Optional chaining to call the function if it exists
+// const obj2: newObj = {
+//   name: "Ashraf Ali",
+//   age: 23,
+//   height: 5.8,
+//   width: 2.5,
+//   isMarried: false,
+//   gender: true,
+// };
+// ! ---- TypeScript Function ----
+// type FuncType = (n: number, m: number) => number;
+// const func: FuncType = (a, b) => {
+//   return a - b;
+// }; // a and b and n and m are same, but we can use any name
+// ? Optional Parameters in TypeScript
+// type FuncType = (n: number, m: number, l?: number) => number;
+// const func: FuncType = (n, m, l) => {
+//   if (l === undefined) {
+//     return n * m; // If l is not provided, return n * m
+//   }
+//   return n * m * l;
+// };
+// console.log(func(10, 20));
+// console.log(func(10, 20, 30));
+// ? Default Parameters in TypeScript
+// const func = (n: number, m: number, l: number = 18) => {
+//   return n * m * l; // If l is not provided, it defaults to 18
+// };
+// console.log(func(23, 35));
+// TODO: Another Way
+// type FuncType = (n: number, m: number, l?: number) => number;
+// const func2: FuncType = (n, m, l = 20) => {
+//   return n * m * l;
+// };
+// console.log(func2(23, 35));
+// ? Rest Operator
